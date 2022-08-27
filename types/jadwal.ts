9f685554731544a0cdd10174f3cb35d86f57fa2e
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2022-08-27 11:28:17 +0700

    chore: menambahkan penjelasan kode

--- types/jadwal.ts
+++ types/jadwal.ts
@@ -1,15 +1,23 @@
 import { z } from "zod";
 
+// Nilai yang merepresentasikan hari dari angka 1 (senin)
+// Sampai angka 7 (minggu)
 const dayValidation = z.number().positive().min(1).max(7);
 
 // jadwal.json file typing start //
 export const JadwalJson = z
   .array(
     z.object({
+      // Nama masing-masing kelas.
+      // Harus berbeda satu dengan lainnya.
       className: z.string(),
+
       schedule: z.array(
         z.object({
           day: dayValidation,
+
+          // Array yang berisikan string jadwal
+          // pelajaran siswa pada hari spesifik
           lessons: z.array(z.string()).nonempty(),
         })
       ),
@@ -31,8 +39,15 @@ export const WaktuJson = z.object({
       z.object({
         alloc: z.array(
           z.object({
+            // Nilai dalam object ini pasti akan ada di antara
+            // JAM atau isBreak. Jam akan menjelaskan pada jam
+            // keberapa pelajaran berlangsung, sedangkan isBreak
+            // akan menandakan dalam waktu istirahat telah tiba.
+
             JAM: z.number().optional(),
             isBreak: z.boolean().optional(),
+
+            // Properti yang wajib ada
             WAKTU: z.array(z.string()),
           })
         ),
