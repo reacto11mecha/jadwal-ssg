d2229ab6483a474aee115db47c6c4835a4a0672d
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2023-07-23 03:29:15 +0700

    fix: memperbaiki typing dan mengembalikan type checking

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,4 @@
 /// <reference types="astro/client" />
 /// <reference types="vite-plugin-pwa/info" />
 /// <reference types="vite-plugin-pwa/client" />
+/// <reference types="vite-plugin-pwa/react" />
